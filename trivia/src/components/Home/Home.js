d9810cd55277b892893f777c95cfd34d5014cdfb
An <an.tle.914@gmail.com>
2022-07-22 17:35:24 -0400

    some slight progress

--- trivia/src/components/Home/Home.js
+++ trivia/src/components/Home/Home.js
@@ -1,27 +1,27 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import Axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [categoryId, setCategoryid] = useState(null);
 
-    useEffect(() => {
-      Axios.get("https://opentdb.com/api_category.php").then((response) =>
-        setCategories(response.data.trivia_categories)
-      );
-    }, []);
+  useEffect(() => {
+    Axios.get("https://opentdb.com/api_category.php").then((response) =>
+      setCategories(response.data.trivia_categories)
+    );
+  }, []);
 
-    return(
-        <div className="category-container">
-        <h4>Choose your category to start playing!</h4>
-        {categories.map((category) => (
-          <div
-            className="category"
-            value={category.id}
-            onClick={() => console.log(category.id)}
-          >
+  return (
+    <div className="category-container">
+      <h4>Choose your category to start playing!</h4>
+      {categories.map((category) => (
+        <Link to="/quiz">
+          <button value = {category.id} onClick={() => setCategoryid(category.id)}>
             {category.name}
-          </div>
-        ))}
-      </div>
-    )
+          </button>
+        </Link>
+      ))}
+    </div>
+  );
 }
--- trivia/src/components/Quiz/Quiz.js
+++ trivia/src/components/Quiz/Quiz.js
@@ -1,8 +1,17 @@
 import { Link } from 'react-router-dom'
-import { useState } from 'react'
-import App from '../../App'
+import { useEffect } from 'react'
+import Axios from 'axios'
+import Home from '../Home/Home'
 
 export default function Quiz() {
+  useEffect(() => {
+    Axios
+    .get(`https://opentdb.com/api.php?amount=10&category=10`)
+    .then((response) =>
+      console.log(response.data.results)
+    );
+  }, []);
+
     return (
       <div className="quiz">
         <Link to='/'>
@@ -11,7 +20,7 @@ export default function Quiz() {
             </button>
         </Link>
         <button>
-            Submit
+            Submit Answer
         </button>
       </div>
     );
