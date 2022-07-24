7aee5f171c75b89f91e5aee416cce20ae5daa424
An <an.tle.914@gmail.com>
2022-07-24 14:44:52 -0400

    Fixed rendering issue. Need to fix correct answer logic

--- trivia/src/components/Quiz/Quiz.js
+++ trivia/src/components/Quiz/Quiz.js
@@ -6,33 +6,33 @@ export default function Quiz() {
   const [questions, setQuestions] = useState(null);
   const [questionNumber, changeQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0)
 
   const location = useLocation();
   const idLocation = location.pathname;
 
   useEffect(() => {
-    questionFetch(idLocation.slice(6)).then((response) =>
-      setQuestions(response)
-    );
-  }, []);
+    questionFetch(idLocation.slice(6))
+    .then((response) => setQuestions(response))
+  }, [])
 
-  const x = () => {
-    console.log(questions);
-  };
-  x();
+  if (!questions) {
+    return <>Loading page..</>
+  }
 
-  const shuffle = arr => [...arr].sort(() => Math.random() - 0.5);
+  const shuffle = (arr) => [...arr].sort(() => Math.random() - 0.5);
 
   const answerChoices = [
-    (questions[questionNumber].incorrect_answers[0]: false),
-    (questions[questionNumber].incorrect_answers[1] : false),
-    (questions[questionNumber].incorrect_answers[2] : false),
-    (questions[questionNumber].correct_answer : true)
+    questions[questionNumber].incorrect_answers[0],
+    questions[questionNumber].incorrect_answers[1],
+    questions[questionNumber].incorrect_answers[2],
+    questions[questionNumber].correct_answer,
   ];
 
-  const randomAnswerChoices = shuffle(answerChoices)
+  const randomAnswerChoices = shuffle(answerChoices);
 
-  const handleAnswerClick = () =>{ 
+  const handleAnswerClick = () => {
+    setScore(score + 1);
     if (questionNumber != questions.length - 1) {
       changeQuestion(questionNumber + 1);
     } else {
@@ -40,31 +40,42 @@ export default function Quiz() {
     }
   };
 
-  const y = () => {
-    console.log(randomAnswerChoices);
-  };
-  y();
+  // const y = () => {
+  //   console.log(randomAnswerChoices);
+  // };
+  // y();
 
-  const z = () => {
-    console.log(answerChoices);
-  };
-  z();
+  // const z = () => {
+  //   console.log(answerChoices);
+  // };
+  // z();
 
   return (
     <>
       {showScore ? (
-        <div className="score">You got 1 out of 10 right!</div>
+        <div className="score">You got {score} out of 10 right!</div>
       ) : (
         <>
           <h2>Category chosen is : {questions[questionNumber].category}</h2>
           <div className="question">
-            <h3>Question {questionNumber + 1}: {questions[questionNumber].question}</h3>
+            <h3>
+              Question {questionNumber + 1}:{" "}
+              {questions[questionNumber].question}
+            </h3>
           </div>
           <div className="answer-choices">
-            <button onClick={handleAnswerClick}>{randomAnswerChoices[0]}</button>
-            <button onClick={handleAnswerClick}>{randomAnswerChoices[1]}</button>
-            <button onClick={handleAnswerClick}>{randomAnswerChoices[2]}</button>
-            <button onClick={handleAnswerClick}>{randomAnswerChoices[3]}</button>
+            <button onClick={handleAnswerClick}>
+              {randomAnswerChoices[0]}
+            </button>
+            <button onClick={handleAnswerClick}>
+              {randomAnswerChoices[1]}
+            </button>
+            <button onClick={handleAnswerClick}>
+              {randomAnswerChoices[2]}
+            </button>
+            <button onClick={handleAnswerClick}>
+              {randomAnswerChoices[3]}
+            </button>
           </div>
         </>
       )}
